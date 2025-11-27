31953c200ff7e0050c09c839c67b957d4ad3c303
mohammad ehsan nazari <nazariehsan80@gmail.com>
2025-11-27 23:46:51 +0330

    cors problem

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ connectDB();
 
 const app = express();
 
-app.use(cors());
+app.use(cors({ origin: "*", methods: ["GET", "POST", "PUT", "DELETE"] }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(compression());
